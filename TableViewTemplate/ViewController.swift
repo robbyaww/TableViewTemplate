4023e8abc9e6f0c7de6c1b3bf65c27e4556e46bf
robbyaww <robbyawalul@gmail.com>
2020-07-08 13:39:26 +0700

    Initial Commit

---
+++ TableViewTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewTemplate
+//
+//  Created by Robby Awalul Meviansyah Abdillah on 08/07/20.
+//  Copyright © 2020 Robby Awalul Meviansyah Abdillah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
