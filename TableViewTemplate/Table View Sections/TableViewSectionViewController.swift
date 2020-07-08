4ed642a2dda66e21b8dce36298bc07c06f5df6ad
Robby Awalul Meviansyah Abdillah <robbyawalul@gmail.com>
2020-07-08 13:48:37 +0700

    initial commit

---
+++ TableViewTemplate/Table View Sections/TableViewSectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewSectionViewController.swift
+//  TableViewTemplate
+//
+//  Created by Robby Awalul Meviansyah Abdillah on 08/07/20.
+//  Copyright © 2020 Robby Awalul Meviansyah Abdillah. All rights reserved.
+//
+
+import UIKit
+
+class TableViewSectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //initial commit
+    }
+
+}
--- TableViewTemplate/ViewController.swift
+++ TableViewTemplate/ViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var btnNext: UIButton!
+    
+    @IBAction func btnNextAction(_ sender: Any) {
+        let vc = TableViewSectionViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
 }
